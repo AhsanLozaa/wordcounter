ed17d314185833a311aa7925d47dd301e48e1bea
Ahsan Ilyas <ahsanmohamed97@gmail.com>
2019-12-07 16:56:53 +0530

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\Studies\Give A Try\Django\3_Project1_WordCounter\WordCounter\wordcounter\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
